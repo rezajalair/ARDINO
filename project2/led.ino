61bc01f3a7cd8a01e3e38de35cb9ac6935b9503b
rezajalair <rezajalayer83@gmail.com>
2025-03-11 20:06:40 +0330

    project2

--- project2/led.ino
+++ project2/led.ino
@@ -1,15 +1,26 @@
 ```cpp
 
-int led = 13;
- 
-void setup () {
-pinMode (led, OUTPUT); 
-}
-Void loop () {
-digitalWrite (led, HIGH);
-delay (1000);
-digitalWrite (led, LOW);
-delay (1000);
-}
+int led = 13; 
+int button = 7;
+ int state = 0;
+ void setup()
+
+ {
+ pinMode(led, OUTPUT);
+ pinMode(button, INPUT);
+ }
+
+void loop() 
+{ state = digitalRead(button);
+
+ if (state == HIGH) 
+
+{ digitalWrite(led, HIGH); }
+
+ else 
+
+{ digitalWrite(led, LOW); }
+
+ }
 
 ```
